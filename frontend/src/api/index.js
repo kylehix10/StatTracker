8ebda9ae5cfff4b42991efa1f66225d2bd94fb57
KyleHix <khix@email.sc.edu>
2026-05-24 15:13:54 -0400

    index.js route

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE = 'http://localhost:5000/api';
+const BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 
 // Users
 export const getUsers = () => axios.get(`${BASE}/users`);
