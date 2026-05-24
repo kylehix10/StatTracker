64c5ddd80e2ac5cc541290167b64e22e5848edb7
KyleHix <khix@email.sc.edu>
2026-05-24 16:28:10 -0400

    indes.js troubleshoot

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-const BASE = API_URL
-  ? API_URL.replace(/\/$/, '')
-  : 'http://localhost:5000/api';
+//const BASE = API_URL
+  //? API_URL.replace(/\/$/, '')
+ // : 'http://localhost:5000/api';
 
 
 // Users
